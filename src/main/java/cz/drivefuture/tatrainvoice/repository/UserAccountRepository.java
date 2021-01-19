24b1853957033ace837d880e61b9ce4339ae71ea
drivefuture <pavol@drivefuture.cz>
2021-01-19 19:02:41 +0100

    Fix generator changes to repositories

--- src/main/java/cz/drivefuture/tatrainvoice/repository/UserAccountRepository.java
+++ src/main/java/cz/drivefuture/tatrainvoice/repository/UserAccountRepository.java
@@ -1,7 +1,9 @@
 package cz.drivefuture.tatrainvoice.repository;
 
+import cz.drivefuture.tatrainvoice.domain.User;
 import cz.drivefuture.tatrainvoice.domain.UserAccount;
-
+import java.util.Optional;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,9 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+    String USERS_ACCOUNTS_BY_LOGIN_CACHE = "usersAccountsByLogin";
+
+    @EntityGraph(attributePaths = { "currentCompany", "companies", "user", "user.authorities" })
+    //@Cacheable(cacheNames = USERS_ACCOUNTS_BY_LOGIN_CACHE)
+    Optional<UserAccount> findOneWithCompaniesAndUserWithUserAuthoritiesByUserLogin(String login);
 }
